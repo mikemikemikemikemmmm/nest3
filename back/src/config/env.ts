5f0aef9c566425614a9d3f57d2a1898a4b5d5cff
mike <>
2024-02-25 22:08:36 +0800

    fix back env

--- back/src/config/env.ts
+++ back/src/config/env.ts
@@ -25,7 +25,6 @@ export const getSubproductImageFilePath = (spId:string)=>{
 export const getMenuBannerImageFilePath = (menuId:string)=>{
     return join(process.cwd(),"public",`${getNowEnviroment()}`,"menu",`${menuId}.jpg`)
 }
-
 export const getProductImageFilePath = (productId:string,imageName:string)=>{
     return join(process.cwd(),"public",`${getNowEnviroment()}`,"products",productId,`${imageName}.jpg`)
 }
